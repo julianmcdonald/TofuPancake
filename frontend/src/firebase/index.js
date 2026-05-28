f6cadbfb6608ede59750647603c73eeb3ddc568d
Julian McDonald <julian.mcdonald@gmail.com>
2026-05-28 19:12:18 +1000

    feat: mock frontend Firebase SDK to prevent invalid API key crash and default to guest play

--- frontend/src/firebase/index.js
+++ frontend/src/firebase/index.js
@@ -1,9 +1,28 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import firebaseConfig from './firebaseConfig';
+const mockAuth = {
+  onIdTokenChanged: (callback) => {
+    // Instantly notify application that there is no active logged-in user (default to guest mode)
+    callback(null);
+    return () => {};
+  },
+  signInWithPopup: async () => {
+    console.log('Sign in is disabled in guest mode.');
+    return {};
+  },
+  signOut: async () => {
+    console.log('Sign out is disabled in guest mode.');
+    return {};
+  },
+  currentUser: null,
+};
 
-firebase.initializeApp(firebaseConfig);
+const mockFirebase = {
+  auth: Object.assign(() => mockAuth, {
+    GoogleAuthProvider: class {},
+  }),
+  initializeApp: () => {},
+};
 
-const auth = firebase.auth();
+const auth = mockAuth;
+const firebase = mockFirebase;
 
 export { auth, firebase };
